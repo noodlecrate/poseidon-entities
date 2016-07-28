42cd4305f9bfdee202113eac170227f4db59e6d5
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-07-28 13:47:45 +0100

    Set up index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export { }
+import * as DTOsNamespace from './dtos/_namespace';
+import * as ModelsNamespace from './models/_namespace';
+
+export {
+
+    DTOsNamespace as DTOs,
+    ModelsNamespace as Models
+
+};
+ 
